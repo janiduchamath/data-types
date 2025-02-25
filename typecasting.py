ec142d5d76b553f7f125670694ac3e64d35afb82
janidu925 <janiduchama@gmail.com>
2025-02-25 20:22:09 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,25 @@
+name = "penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print("name : ",name)
+print("datatype of object is ",type(name))
+
+print("age : ",age)
+print("datatype of object is ",type(age))
+
+print("is student : ",is_student)
+print("datatype of object is ",type(is_student))
+
+print("weight : ",weight)
+print("datatype of object is ",type(weight))
+
+print("/n after typecasting....")
+age = str(age)
+print(age)
+print("datatype of age is ",type(age))
+weight = int(weight)
+print(weight)
+print("datatype of weight is ",type(weight))
+
